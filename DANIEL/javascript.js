210109454a33bdfeaa32d70df28e2fd6448d35c7
Daniel Kitov <kitovdaniel81@gmail.com>
2026-05-25 13:54:00 +0300

    full js function

--- DANIEL/javascript.js
+++ DANIEL/javascript.js
@@ -22,3 +22,42 @@ const reviews = [
 }
 
 ];
+
+let current = 0;
+
+function showReview(){
+
+    document.getElementById("name").innerText =
+    reviews[current].name;
+
+    document.getElementById("stars").innerText =
+    reviews[current].stars;
+
+    document.getElementById("text").innerText =
+    reviews[current].text;
+
+    document.getElementById("reviewer").src =
+    reviews[current].img;
+}
+
+function nextReview(){
+
+    current++;
+
+    if(current >= reviews.length){
+        current = 0;
+    }
+
+    showReview();
+}
+
+function previousReview(){
+
+    current--;
+
+    if(current < 0){
+        current = reviews.length - 1;
+    }
+
+    showReview();
+}
